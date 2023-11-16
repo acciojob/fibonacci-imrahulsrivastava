74882c2848ff521263635af25a8f1be6d486cfa7
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-16 15:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+  // Write your code here
+
+  if (num === 1 || num === 2) {
+    return num - 1;
+  }
+
+  const arr = [0, 1];
+  let count = num - 2;
+
+  while (count > 0) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    count--;
+  }
+
+  return arr[num - 1];
 }
 
 module.exports = fibonacci;
